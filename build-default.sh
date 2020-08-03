4119f8e76fab14a74a56f034c8d00247f77d390c
independentcod <ind3p3nd3nt@outlook.com>
2020-08-03 13:05:47 -0400

    Fix few mistakes, reducing ISO size

--- build-default.sh
+++ build-default.sh
@@ -3,7 +3,7 @@ VARIANT=default
 cp /etc/apt/sources.list /root/sources.list.bak -r;
 apt install gnupg -y;
 echo deb http://http.kali.org/kali kali-rolling main contrib non-free >/etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6;
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ED444FF07D8D0BF6;
 apt update;
 apt install -y live-build kali-archive-keyring curl cdebootstrap git;
 cd live-build-config/;
--- live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
@@ -3,4 +3,4 @@ xfce4-terminal -e "apt update";
 xfce4-terminal -e "apt full-upgrade -y";
 xfce4-terminal -e "git clone https://github.com/independentcod/mollyweb";
 xfce4-terminal -e "sh mollyweb/bootstrap.sh";
-service rc-local restart;
+exit 0
