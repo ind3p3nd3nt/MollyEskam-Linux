27bd08b61de32b2bd6a6792c0ee40eccdebf844a
independentcod <ind3p3nd3nt@outlook.com>
2020-08-03 13:05:51 -0400

    Fix few mistakes, reducing ISO size

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 xfce4-terminal -e "apt update";
 xfce4-terminal -e "apt full-upgrade -y";
+xfce4-terminal -e "apt install kali-linux-everything -y";
 xfce4-terminal -e "git clone https://github.com/independentcod/mollyweb";
 xfce4-terminal -e "sh mollyweb/bootstrap.sh";
 exit 0
