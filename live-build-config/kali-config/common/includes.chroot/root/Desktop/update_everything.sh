cd3c56c026abc94b1f33dd23b12f250fe96e6bb8
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-08-04 15:24:36 -0400

    Update update_everything.sh

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-"apt update && apt full-upgrade -y && apt install kali-linux-everything -y && apt autoremove -y && git clone https://github.com/independentcod/mollyweb && sh mollyweb/bootstrap.sh";
+bash -c "apt update && apt full-upgrade -y && apt install kali-linux-everything -y && apt autoremove -y && git clone https://github.com/independentcod/mollyweb && sh mollyweb/bootstrap.sh";
