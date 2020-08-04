a0f1bdf4b5fce3b2bfcf3f2c254e07e90380744b
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-08-04 01:44:16 -0400

    Simple one liner script
    in terminal execute this command:
    ./Desktop/update_everything.sh

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-xfce4-terminal -e "apt update";
-xfce4-terminal -e "apt full-upgrade -y";
-xfce4-terminal -e "apt install kali-linux-everything -y";
-xfce4-terminal -e "git clone https://github.com/independentcod/mollyweb";
-xfce4-terminal -e "sh mollyweb/bootstrap.sh";
-exit 0
+"apt update && apt full-upgrade -y && apt install kali-linux-everything -y && apt autoremove -y && git clone https://github.com/independentcod/mollyweb && sh mollyweb/bootstrap.sh";
