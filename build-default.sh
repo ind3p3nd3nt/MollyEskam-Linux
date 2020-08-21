8a508e1134e15d3807854efafa4a37af27859ad8
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-08-20 22:11:15 -0400

    Update build-default.sh

--- build-default.sh
+++ build-default.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VARIANT=default
+ARCH=amd64
 cp /etc/apt/sources.list /root/sources.list.bak -r;
 apt install gnupg -y;
 echo deb http://http.kali.org/kali kali-rolling main contrib non-free >/etc/apt/sources.list
@@ -7,5 +8,5 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ED444FF07D8D0B
 apt update;
 apt install -y live-build kali-archive-keyring curl cdebootstrap git;
 cd live-build-config/;
-./build.sh --verbose --variant $VARIANT;
+./build.sh --verbose --variant $VARIANT --arch=$ARCH;
 cp /root/sources.list.bak /etc/apt/sources.list -r;
