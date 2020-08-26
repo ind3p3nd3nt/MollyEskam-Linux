896b3d4720260d81f725a752c6cd3b4df6d96267
root <root@kali>
2020-08-25 20:52:26 -0400

    Added config for KDE Plasma as default flavour

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/update_everything.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "Updating your distribution please do not close this window until operation complete.";
-bash -c "apt update && apt full-upgrade -y && apt install kali-linux-everything -y && apt autoremove -y && git clone https://github.com/independentcod/mollyweb && sh mollyweb/bootstrap.sh";
+xfce4-terminal -e "/sbin/update";
